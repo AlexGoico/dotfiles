e6f6d798dceaad5b2fcf858e6fbdfd76eb66b6ea
typedeph <alexegoico@gmail.com>
2015-09-26 04:49:26 -0400

    Removed quicklisp since not currently using it

--- .emacs.d/my_lisp/common-lisp_prefs.el
+++ .emacs.d/my_lisp/common-lisp_prefs.el
@@ -5,9 +5,3 @@
 (add-hook 'slime-repl-mode-hook 'set-up-slime-ac)
 (eval-after-load "auto-complete"
    '(add-to-list 'ac-modes 'slime-repl-mode))
-
-
-
-(load (expand-file-name "~/quicklisp/slime-helper.el"))
-  ;; Replace "sbcl" with the path to your implementation
-  (setq inferior-lisp-program "sbcl")
